4f2f1d858e5378df113fd32c3b475ae2f71f39d4
jonschlinkert <dev@sellside.com>
2014-11-29 01:49:32 -0500

    use reduce, add error handling

--- index.js
+++ index.js
@@ -3,34 +3,33 @@
 var extract = require('esprima-extract-comments');
 var minimist = require('minimist');
 
-module.exports = function commendments(keywords, str) {
-  var o = {missing: [], omit: []};
+module.exports = function commandments(keywords, str) {
+  if (!Array.isArray(keywords) && typeof keywords !== 'string') {
+    throw new TypeError('commandments expects a string or array as the first argument.');
+  }
+
+  if (typeof str !== 'string') {
+    throw new TypeError('commandments expects a string as the second argument.');
+  }
 
   keywords = !Array.isArray(keywords)
     ? [keywords]
     : keywords
 
-  var id = keywords.join('|');
-  var re = new RegExp('\\s*(?:' + id + '):([^\\*]+)');
-  if (!str) {
-    return [];
-  }
+  var re = new RegExp('\\s*(?:' + keywords.join('|') + '):([^\\*]+)');
+  var o = {missing: [], omit: []};
 
-  var comments = extract.fromString(str);
-  var commands = {};
-
-  keywords.forEach(function(keyword) {
-    comments.forEach(function(ele) {
+  return keywords.reduce(function(acc, keyword) {
+    extract.fromString(str).forEach(function(ele) {
       var comment = ele.value.trim();
 
       if(new RegExp('^' + keyword).test(comment)) {
         var match = re.exec(comment);
-        if (match != null) {
-          commands[keyword] = minimist(match[1].trim().split(' '));
+        if (match) {
+          acc[keyword] = minimist(match[1].trim().split(' '));
         }
       }
-    })
-  });
-
-  return commands;
+    });
+    return acc;
+  }, {});
 };

d6044960e203cf0b19ce2a3237ec5a6027f0e3dd
jonschlinkert <dev@sellside.com>
2014-11-29 02:25:10 -0500

    remove example

--- example.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var commands = require('./');
-
-
-var str = fs.readFileSync('fixtures/fixture.js', 'utf-8')
-var fixture = commands(['abc', 'xyz'], str);
-console.log(fixture)

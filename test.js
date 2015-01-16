718502841e71f597f65fc2631c9c172644082575
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-16 03:17:04 -0500

    update copyright

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /*!
  * commandments <https://github.com/jonschlinkert/commandments>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Copyright (c) 2014-2015, Jon Schlinkert.
  * Licensed under the MIT License
  */
 
